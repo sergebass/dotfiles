415098756b3459460ee13e6b98e9bc9c301c4d16
Sergii Perynskyi <sergiip@annidis.com>
2017-04-11 21:53:40 -0400

    Add (hardcoded) Idris binary path from .cabal to .emacs.d/init.el.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -65,6 +65,7 @@
  '(column-number-mode t)
  '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (deeper-blue)))
+ '(idris-interpreter-path "/home/sergii/.cabal/bin/idris")
  '(package-selected-packages
    (quote
     (rtm idris-mode rust-mode haskell-emacs highlight-symbol haskell-mode popup-complete jedi dired+ buffer-move buffer-flip wgrep bookmark+ rainbow-delimiters minimap auto-complete flycheck rtags magit powerline-evil window-numbering evil elpy clojure-mode-extra-font-locking clojure-cheatsheet)))
