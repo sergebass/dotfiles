788ad882fafc65721cb3c471468c5d594d76e332
Sergii Perynskyi <sergebass@gmail.com>
2018-02-06 10:46:20 -0500

    get-pulseaudio-volume.sh: list volume levels for all devices, not just one.

--- scripts/get-pulseaudio-volume.sh
+++ scripts/get-pulseaudio-volume.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*%\).*,\1,'
+pactl list sinks | grep '^[[:space:]]Volume:' | sed -e 's,.* \([0-9][0-9]*%\).*,\1,' | tr '\n' ' '
