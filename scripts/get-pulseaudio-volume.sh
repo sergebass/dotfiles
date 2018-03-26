fcb57418458c71d0588dfc1822bea6c33f5e4fe9
Sergii Perynskyi <sergebass@gmail.com>
2018-03-26 15:49:45 -0400

    scripts/get-pulseaudio-volume.sh: trim whitespace from ends (to save space in xmobar status bar).

--- scripts/get-pulseaudio-volume.sh
+++ scripts/get-pulseaudio-volume.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pactl list sinks | grep '^[[:space:]]Volume:' | sed -e 's,.* \([0-9][0-9]*%\).*,\1,' | tr '\n' ' '
+pactl list sinks | grep '^[[:space:]]Volume:' | sed -e 's,.* \([0-9][0-9]*%\).*,\1,' | tr '\n' ' ' | xargs
