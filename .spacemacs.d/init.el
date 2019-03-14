67c1e23aef62ef90e93887bb2eb3caa42c3bc6d3
Sergii Perynskyi <sergebass@gmail.com>
2019-03-14 18:21:00 -0400

    spacemacs: jump on the bandwagon of experimenting with <BS> bound to ^ as well.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -316,7 +316,7 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
-  (define-key evil-normal-state-map (kbd "DEL") evil-window-map)
+  (define-key evil-normal-state-map (kbd "DEL") (kbd "^"))
 
   ;; free up the normal backslash for to be our vim-like <Leader>/<LocalLeader>,
   ;; rebind to use C-\ instead for the evil-execute-in-emacs-state
