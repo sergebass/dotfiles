3fffe8eaba2363e50fd25b1f3674a2eee20da588
Sergii Perynskyi <sergebass@gmail.com>
2019-05-29 14:51:13 -0400

    spacemacs: make line numbers dimmer.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -410,4 +410,5 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:foreground "#E0E0E0" :background "#202020" :family "Inconsolata" :foundry "CYRE" :slant normal :weight normal :height 105 :width normal)))))
+ '(default ((t (:foreground "#E0E0E0" :background "#202020" :family "Inconsolata" :foundry "CYRE" :slant normal :weight normal :height 105 :width normal))))
+ '(linum ((t (:background "#000000" :foreground "#777777")))))
