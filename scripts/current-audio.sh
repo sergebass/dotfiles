b23e17ffe3352252b5cf82a88869cba4332a8db8
Sergii Perynskyi <sergebass@gmail.com>
2019-10-24 12:36:34 -0400

    current-audio.sh: simplify the sed whitespace trimming expression

--- scripts/current-audio.sh
+++ scripts/current-audio.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # the sed command is here to trim redundant whitespace (as emitted by some radio stations)
-(mpc -f '[%title% ][@%name%]' current || echo '--') | sed 's/ \{1,\}/ /g'
+(mpc -f '[%title% ][@%name%]' current || echo '--') | sed 's/  \+/ /g'
