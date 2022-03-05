93137895e30d52817840dfe63dcd0e3b8dcf327a
Sergii Perynskyi <sergebass@gmail.com>
2022-03-04 19:07:41 -0500

    nixos: enable Bluetooth and Blueman

--- nixos/configuration-n21.nix
+++ nixos/configuration-n21.nix
@@ -58,6 +58,10 @@
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
+  # Enable Bluetooth
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
 
