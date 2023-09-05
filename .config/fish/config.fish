a8a421cb90905c178a457051cc4b62aa311453aa
Sergii Perynskyi <sergebass@gmail.com>
2023-09-05 13:23:58 -0400

    fish: add ~/.cargo/bin to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 if type -q fish_add_path
     fish_add_path --prepend --move ~/.local/bin
     fish_add_path --prepend --move ~/bin
+
+    fish_add_path --append --move ~/.cargo/bin
 end
 
 if status is-interactive
