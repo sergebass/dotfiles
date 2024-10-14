2ae1683f75776846923226bcc77a01930abed87f
Sergii Perynskyi <sergebass@gmail.com>
2024-10-14 17:24:42 -0400

    NixOS (vostro-3350): allow printing from other computers on LAN

--- nixos/vostro3350/configuration.nix
+++ nixos/vostro3350/configuration.nix
@@ -209,11 +209,30 @@ in {
 
     printing = {
       enable = true;  # Enable CUPS
-      cups-pdf.enable = true;
-      browsing = true;  # Advertise shared printers on our LAN
+      startWhenNeeded = true;  # Enable printing on-demand only
+      webInterface = true;  # Enable CUPS web UI as well
+      browsing = true;  # Advertise local printers on our LAN
+      browsed.enable = false;  # But do not browse for external printers (https://www.evilsocket.net/2024/09/26/Attacking-UNIX-systems-via-CUPS-Part-I/)
+      listenAddresses = [ "*:631" ];  # The CUPS port
+      allowFrom = [ "all" ];
+      openFirewall = true;  # Open ports for listenAddresses
+      defaultShared = true;  # Local printers are shared by default.
+      cups-pdf.enable = true;  # Enable the virtual PDF printer backend
       drivers = with pkgs; [
         brlaser  # For Brother DCP-7060D (and others)
       ];
+    };
+
+    # Enable auto-discovery (of local printers in particular)
+    avahi = {
+      enable = true;
+      openFirewall = true;  # Discovery is done via the opened UDP port 5353
+      nssmdns4 = true;  # Enable the mDNS NSS (Name Service Switch) plug-in for IPv4. Enabling it allows applications to resolve names in the .local domain by transparently querying the Avahi daemon.
+      nssmdns6 = true;  # Same for IPv6
+      publish = {
+        enable = true;
+        userServices = true;
+      };
     };
 
     udev.packages = [ pkgs.rtl-sdr ];
@@ -285,6 +304,7 @@ in {
       "adm"
       "audio"
       "lp"  # Printer access
+      "lpadmin"  # Printer administration
       "networkmanager"
       "plugdev"  # For e.g. RTL-SDR
       "scanner"
@@ -396,6 +416,7 @@ in {
       sox
       speedtest-cli
       sway-launcher-desktop  # Note that this tool is not really tied to sway at all
+      system-config-printer  # Printer configuration/pipeline control UI
       thunderbird
       tig
       tmux
