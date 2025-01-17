cf75f88f90316c252fa9dd844122327b8366fc49
Sergii Perynskyi <sergebass@gmail.com>
2025-01-17 18:54:55 -0500

    .workspace.fish: add a stub for workspace-specific fish shell configuration

---
+++ workspace-templates/.workspace.fish
@@ -0,0 +1,10 @@
+# ------ workspace-specific environment configuration -----
+
+# export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+
+# if type -q fish_add_path
+#     fish_add_path --prepend --move ~/.local/bin
+#     fish_add_path --prepend --move ~/bin
+
+#     fish_add_path --append --move ~/.cargo/bin
+# end
--- workspace-templates/.workspace.sh
+++ workspace-templates/.workspace.sh
@@ -1,3 +1,5 @@
 # ------ workspace-specific environment configuration -----
 
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+# export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+
+# export PATH=$HOME/bin:$HOME/.local/bin:$PATH
