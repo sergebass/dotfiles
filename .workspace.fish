31e5dd0e685c314972cb441c3617e86595f5b135
Sergii Perynskyi <sergebass@gmail.com>
2025-02-13 12:23:20 -0500

    .workspace.sh and .workspace.fish: add stubs

---
+++ .workspace.fish
@@ -0,0 +1,7 @@
+# ------ workspace-specific environment configuration -----
+
+# export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
+# if type -q fish_add_path
+#     fish_add_path --prepend --move $HOME/bin
+# end
---
+++ .workspace.sh
@@ -0,0 +1,5 @@
+# ------ workspace-specific environment configuration -----
+
+# export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
+# export PATH=$HOME/bin:$PATH
