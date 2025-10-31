899521b2770d25b717229d21bebcae3cf728e01f
Sergii Perynskyi <sergebass@gmail.com>
2025-10-31 13:26:24 -0400

    NixOS: use DHCP for all network interfaces by default

--- nixos/common.nix
+++ nixos/common.nix
@@ -61,6 +61,14 @@
       enable = true;
     };
 
+    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+    # (the default) this is the recommended approach. When using systemd-networkd it's
+    # still possible to use this option, but it's recommended to use it in conjunction
+    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+    # interfaces.enp1s0.useDHCP = lib.mkDefault true;
+    # interfaces.wlp3s0.useDHCP = lib.mkDefault true;
+    useDHCP = lib.mkDefault true;
+
     networkmanager = {
       enable = true;
 
