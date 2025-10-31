51d0a11a95f5b243f7c56160aca1f98f9ef71522
Sergii Perynskyi <sergebass@gmail.com>
2025-10-31 17:17:09 -0400

    NixOS: *wm: import gui-common.nix explicitly

--- nixos/gui-icewm.nix
+++ nixos/gui-icewm.nix
@@ -1,6 +1,7 @@
 # NixOS configuration for IceWM GUI.
 { config, lib, pkgs, ... } : {
   imports = [
+    ./gui-common.nix  # common GUI configuration shared by all of our graphical environments
     ./gui-common-x11.nix  # common configuration shared across X11-based environments
   ];
 
