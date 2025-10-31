fc77ba5e91c68e174c7be8d051299034811c5e77
Sergii Perynskyi <sergebass@gmail.com>
2025-10-30 22:01:52 -0400

    NixOS (vostro3350): refactor to use shared modules

--- nixos/vostro3350/configuration.nix
+++ nixos/vostro3350/configuration.nix
@@ -2,141 +2,29 @@
 
 { config, lib, pkgs, ... }:
 let
-  userName = "sergii";
-  userId = 1000;
-
-  aliases = {
-    v = "$VISUAL";
-    g = "git";
-    rg = "rg -L --sort path --no-heading -n --column";
-    x11 = "startx (which i3)";
-  };
-
-  useDM = true;
+  useDisplayManager = true;
 
 in {
   imports = [
-    # Include the results of the hardware scan.
-    # To redo hardware detection: nixos-generate-config
-    ./hardware-configuration.nix
+    ./hardware-configuration.nix  # Results of the hardware scan. To redo detection: nixos-generate-config
+    ../boot-grub.nix  # This old laptop does not use UEFI, so stick with GRUB2
+    ../common.nix  # Common configuration shared by all of our NixOS systems
+    ../gui-xfce.nix  # XFCE X11/GUI environment
+    ../gui-i3.nix  # i3 X11/GUI environment
+    ../gui-icewm.nix  # IceWM X11/GUI environment
+    ../gui-wayland.nix
+    ../mpd.nix
+    ../oscilloscope.nix
+    ../printing.nix
+    ../scanning.nix
+    ../sdr.nix
+    ../tv.nix
+
   ];
-
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  boot = {
-    loader = {
-      # This old laptop does not use UEFI, so stick with GRUB2
-      grub = {
-        enable = true;
-        device = "/dev/sda";
-      };
-    };
-
-    kernelParams = lib.mkForce [ "verbose" "nosplash" ];
-
-    kernel.sysctl = {
-      "kernel.sysrq" = 1;  # Enable all SysRq functions
-    };
-
-    tmp.useTmpfs = true;  # Save SSD from some wear and tear
-
-    supportedFilesystems = [
-      "btrfs"
-      "zfs"
-      "exfat"
-      "ntfs"
-      "ntfs3"
-    ];
-
-    zfs = {
-      forceImportRoot = false;
-    };
-  };
-
-  hardware = {
-    pulseaudio.enable = false;  # Use PipeWire instead
-
-    bluetooth = {
-      enable = true;
-      powerOnBoot = true;
-      package = pkgs.bluez;
-    };
-
-    # Scanner support
-    sane = {
-      enable = true;
-      brscan4.enable = true;  # Brother DCP-7060D scanner
-    };
-
-    rtl-sdr.enable = true;
-  };
-
-  security = {
-    rtkit.enable = true;
-
-    sudo = {
-      enable = true;
-
-      # Do not require password for rebooting or powering the device off
-      extraRules = [{
-        commands = [
-          {
-            command = "${pkgs.systemd}/bin/reboot";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "${pkgs.systemd}/bin/poweroff";
-            options = [ "NOPASSWD" ];
-          }
-        ];
-        groups = [ "wheel" ];
-      }];
-    };
-  };
 
   networking = {
     hostName = "vostro3350";
     hostId = "9f7ff30f";
-
-    firewall = {
-      enable = true;
-      # allowedTCPPorts = [
-      #   631  # CUPS
-      # ];
-      # allowedUDPPorts = [
-      #   631  # CUPS
-      # ];
-    };
-
-    networkmanager = {
-      enable = true;
-
-      # Prefer our own DNS server list
-      insertNameservers = [
-        "9.9.9.9"  # Quad9 DNS resolver (https://en.wikipedia.org/wiki/Quad9)
-        "8.8.8.8"  # Google Public DNS (https://en.wikipedia.org/wiki/Google_Public_DNS)
-        "8.8.4.4"  # Google Public DNS (https://en.wikipedia.org/wiki/Google_Public_DNS)
-        "1.1.1.1"  # CloudFlare DNS (https://en.wikipedia.org/wiki/1.1.1.1)
-        "1.0.0.1"  # CloudFlare DNS (https://en.wikipedia.org/wiki/1.1.1.1)
-      ];
-
-      wifi.powersave = true;
-    };
-  };
-
-  time.timeZone = "Canada/Eastern";
-
-  i18n.defaultLocale = "en_CA.UTF-8";
-
-  console = {
-    earlySetup = true;
-    font = "ter-i16b";
-    packages = with pkgs; [ terminus_font ];
-    keyMap = "us";
-    # useXkbConfig = true; # use xkbOptions in tty.
   };
 
   services = {
@@ -148,389 +36,52 @@ in {
       '';
     };
 
-    getty = {
-      greetingLine = ''\e{bold}\e{green}NixOS ${config.system.nixos.label}-\m \e{lightmagenta} \n \e{yellow} \l \e{reset}'';
-    };
-
-    openssh = {
-      enable = true;
-      settings.X11Forwarding = true;
-    };
-
-    timesyncd.enable = true;  # Enable NTP
-
-    gpm.enable = true;
-
     libinput.enable = true;  # Enable touchpad support
 
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;  # Emulate PulseAudio
-      jack.enable = true;
-    };
-
-    gvfs.enable = true;  # Mount, trash, and other functionalities
-
     displayManager = {
-        defaultSession = "xfce";
-    };
-
-    xserver = {
-      enable = true;
-
-      displayManager = {
-        startx.enable = ! useDM;
-
-        lightdm = {
-          enable = useDM;
-          greeter.enable = true;
-          background = /home/${userName}/.background-image;
-        };
-      };
-
-      desktopManager = {
-        xterm.enable = false;
-        xfce.enable = true;
-        wallpaper = {
-          mode = "max";
-          combineScreens = false;
-        };
-      };
-
-      windowManager.i3 = {
-        enable = true;
-        extraPackages = with pkgs; [
-          i3status
-          i3lock
-        ];
-      };
-
-      xkb = {
-        layout = "us(altgr-intl),ua";
-        options = "grp:shift_caps_toggle,grp:shifts_toggle,grp_led:scroll,caps:escape,compose:rctrl-altgr,terminate:ctrl_alt_bksp";
-      };
-    };
-
-    blueman.enable = true;
-
-    system-config-printer.enable = true;
-
-    printing = {
-      enable = true;  # Enable CUPS
-      startWhenNeeded = true;  # Enable printing on-demand only
-      webInterface = true;  # Enable CUPS web UI as well
-      browsing = true;  # Advertise local printers on our LAN
-      browsed.enable = false;  # But do not browse for external printers (https://www.evilsocket.net/2024/09/26/Attacking-UNIX-systems-via-CUPS-Part-I/)
-      listenAddresses = [
-        "*:631"
-      ];
-      allowFrom = [
-        "all"
-      ];
-      openFirewall = true;  # Open ports for listenAddresses
-      defaultShared = true;  # Local printers are shared by default.
-      cups-pdf.enable = true;  # Enable the virtual PDF printer backend
-      drivers = with pkgs; [
-        brlaser  # For Brother DCP-7060D (and others)
-      ];
-    };
-
-    # Enable auto-discovery (of local printers in particular)
-    avahi = {
-      enable = true;
-      openFirewall = true;  # Discovery is done via the opened UDP port 5353
-      nssmdns4 = true;  # Enable the mDNS NSS (Name Service Switch) plug-in for IPv4. Enabling it allows applications to resolve names in the .local domain by transparently querying the Avahi daemon.
-      nssmdns6 = true;  # Same for IPv6
-      publish = {
-        enable = true;
-        userServices = true;
-      };
-    };
-
-    udev.packages = [ pkgs.rtl-sdr ];
-
-    # Music Player Daemon
-    mpd = {
-      enable = true;
-      startWhenNeeded = true;  # systemd feature: only start MPD service upon connection to its socket
-
-      user = userName;  # Do not run as root
-
-      network = {
-        listenAddress = "any";  # Allow non-localhost connections
-        port = 6600;  # port 6600 is the default
-      };
-
-      musicDirectory = "/home/${userName}/music";
-      playlistDirectory = "/home/${userName}/music/playlists";
-
-      extraConfig = ''
-        audio_output {
-          type "pipewire"
-          name "PipeWire"
-        }
-
-        audio_output {
-          type "pulse"
-          name "PulseAudio"
-          # server "remote_server" # optional
-          # sink "remote_server_sink" # optional
-        }
-
-        # Audio output for use on Linux
-        audio_output {
-          type "alsa"
-          name "ALSA (default)"
-          # device "hw:0,0" # optional
-          # format "44100:16:2" # optional
-          # mixer_type "hardware" # optional
-          # mixer_device "default" # optional
-          # mixer_control "PCM" # optional
-          # mixer_index "0" # optional
-        }
-      '';
+      defaultSession = "xfce";
     };
   };
-
-  xdg.portal = {
-    enable = true;  # Enable xdg desktop integration (https://github.com/flatpak/xdg-desktop-portal).
-    xdgOpenUsePortal = true;  # Sets environment variable NIXOS_XDG_OPEN_USE_PORTAL to 1. This will make xdg-open use the portal to open programs...
-    wlr.enable = true;  # Enable desktop portal for wlroots-based desktops.
-    lxqt.enable = true;  # Enable the desktop portal for the LXQt desktop environment.
-
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gnome  # Backend implementation for xdg-desktop-portal for the GNOME desktop environment
-      xdg-desktop-portal-gtk  # Desktop integration portals for sandboxed apps
-      xdg-desktop-portal-xapp  # Backend implementation for xdg-desktop-portal for Cinnamon, MATE, Xfce
-    ];
-  };
-
-  systemd.services.mpd.environment = {
-    # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
-    XDG_RUNTIME_DIR = "/run/user/${toString userId}"; # MPD will look inside this directory for the PipeWire socket.
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users = let
-    powerUserGroups = [
-      "adm"
-      "audio"
-      "lp"  # Printer access
-      "lpadmin"  # Printer administration
-      "networkmanager"
-      "plugdev"  # For e.g. RTL-SDR
-      "scanner"
-      "video"
-      "wheel"  # Enable 'sudo' for the user.
-    ];
-  in {
-    mutableUsers = true;
-
-    defaultUserShell = pkgs.fish;
-
-    users = {
-      "${userName}" = {
-        uid = userId;
-        isNormalUser = true;
-        extraGroups = powerUserGroups;
-      };
-    };
-  };
-
-  # We are OK with not completely free packages in our system
-  nixpkgs.config.allowUnfree = true;
 
   environment = {
     # List packages installed in system profile. To search, run:
     # $ nix search wget
     systemPackages = with pkgs; [
-      adbfs-rootless  # Mount Android phones on Linux with adb, no root required
-      alacritty
       audacity
-      baobab  # Used disk space visualizer
-      bc
       calibre
-      chromium
       coltrane
-      dig
-      dmenu
-      dunst
-      exfatprogs
-      fastfetch  # Like neofetch, but much faster because written in C
-      feh
       ffmpeg
-      file
-      findutils
-      firefox
-      fish
-      freetube
       frescobaldi
-      fzf
       geeqie
-      git
       glmark2
       gnome-maps
-      gnuradio
-      gparted
-      gqrx
       guitarix
       gxplugins-lv2
-      hddtemp  # HDD temperature measurement tools
-      htop
       hydrogen
       jackmeter
-      kbd
-      libnotify
       libreoffice
       light # Control backlight brightness
       lilypond-with-fonts
-      lm_sensors  # Onboard sensor measurement tools
-      lshw
       luajit
-      mc
       meterbridge
       mi2ly
       mpc_cli
-      mpd
-      mpv
-      mupdf
       musescore
-      ncmpcpp
-      neovim
-      networkmanagerapplet  # Provides nm-connection-editor
-      nushell
-      open-in-mpv  # Simple web extension to open videos in mpv
-      pamixer  # CLI utilities to control PA & PW audio levels
-      parted
-      pavucontrol
-      pciutils
       qjackctl
       qpwgraph
       qrencode
       qsynth
       rakarrack
-      ripgrep
-      rofi
-      rofi-bluetooth
-      rofi-calc
-      rofi-file-browser
-      rofi-mpd
-      rofi-pulse-select
-      rofi-systemd
-      rofi-top
-      rtl-sdr
-      sane-frontends  # Scanner Access Now Easy
-      scrot
       signal-cli
       signal-desktop
-      simple-scan  # Simple paper scanning GUI app
-      sound-theme-freedesktop  # Freedesktop reference sounds
       sox
       speedtest-cli
-      sway-launcher-desktop  # Note that this tool is not really tied to sway at all
-      system-config-printer  # Printer configuration/pipeline control UI
       thunderbird
-      tig
-      tmux
-      tree
       tuxguitar
-      universal-ctags
-      unzip
-      usbutils
-      vifm  # Vi-like File Manager
-      vlc
-      wget
-      xdg-dbus-proxy  # DBus proxy for Flatpak and others
-      xdg-launch  # A command line XDG compliant launcher and tools
-      xdg-terminal-exec  # Reference implementation of the proposed XDG Default Terminal Execution Specification
-      xdg-user-dirs  # A tool to help manage well known user directories like the desktop folder and the music folder
-      xdg-utils  # A set of command line tools that assist applications with a variety of desktop integration tasks
-      xfce.xfce4-sensors-plugin  # Panel plug-in for different sensors using acpi, lm_sensors and hddtemp
-      xorg.xkill
-      xorg.xrandr
-      xorg.xsetroot
       xsane  # Graphical scanning frontend for sane
       ymuse  # GUI client for MPD
       youtube-tui
-      zfs
-      zip
     ];
-
-    variables = rec {
-      EDITOR = "nvim";
-      VISUAL = EDITOR;
-      PAGER = "less";
-      LESS = "-FRX";
-    };
-
-    localBinInPath = true;
-    homeBinInPath = true;
-  };
-
-  programs = {
-    gnupg.agent = {
-      enable = true;
-      enableSSHSupport = true;
-    };
-
-    # Some programs need SUID wrappers, can be configured further
-    # or are started in user sessions.
-    # mtr.enable = true;
-
-    fish = {
-      enable = true;
-      shellAliases = aliases;
-      loginShellInit = ''
-        set -U fish_greeting ""
-
-        if test "$(tty)" = "/dev/tty1";
-          ${pkgs.fastfetch}/bin/fastfetch
-        end
-      '';
-    };
-
-    # Sway is a Wayland compositor
-    sway = {
-      enable = true;
-      extraPackages= with pkgs; [
-        grim
-        mako  # Pop-up notifications
-        rofi-wayland
-        slurp
-        swaybg
-        swayfx
-        swayidle
-        swayimg
-        swaylock
-        wl-clipboard
-      ];
-      extraOptions = [
-        "--verbose"
-      ];
-      extraSessionCommands = ''
-        # Some apps need explicit hints to choose Wayland over X11
-        export GDK_BACKEND=wayland
-        export SDL_VIDEODRIVER=wayland
-        export QT_QPA_PLATFORM=wayland
-        export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-        export _JAVA_AWT_WM_NONREPARENTING=1
-        export NIXOS_OZONE_WL=1
-        export ELECTRON_OZONE_PLATFORM_HINT=wayland
-        export MOZ_ENABLE_WAYLAND=1
-      '';
-      wrapperFeatures.gtk = true;
-    };
-
-    thunar = {
-      enable = true;
-      plugins = with pkgs.xfce; [
-        thunar-archive-plugin
-        thunar-media-tags-plugin
-        thunar-volman
-      ];
-    };
   };
 
   # Copy the NixOS configuration file and link it from the resulting system
