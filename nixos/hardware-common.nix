1374984e3e3302067b8582f9b5f644650aaa8410
Sergii Perynskyi <sergebass@gmail.com>
2025-10-31 21:21:28 -0400

    NixOS: Fix a typo: s/btrs/btrfs-list-failed-{inodes,files}

--- nixos/hardware-common.nix
+++ nixos/hardware-common.nix
@@ -1,11 +1,11 @@
 # NixOS hardware configuration shared across multiple systems.
 
 { config, lib, pkgs, modulesPath, ... }: let
-  btrfsListFailedInodesScript = with pkgs; writeShellScriptBin "btrs-list-failed-inodes" ''
+  btrfsListFailedInodesScript = with pkgs; writeShellScriptBin "btrfs-list-failed-inodes" ''
     dmesg | grep -Po 'csum failed root \d+ ino\S* \d+' | awk '{print $6}' | sort -nu
   '';
 
-  btrfsListFailedFilesScript = with pkgs; writeShellScriptBin "btrs-list-failed-files" ''
+  btrfsListFailedFilesScript = with pkgs; writeShellScriptBin "btrfs-list-failed-files" ''
     # $1: root of the filesystem/directory tree where the failed files are searched
 
 dmesg | grep -Po 'csum failed root \d+ ino\S* \d+' | awk '{print $6}' | sort -nu | xargs -n 1 find $1 -inum 2> /dev/null
