adb78ae2fd21472cd45b2500c46b1d41af6eace5
Sergii Perynskyi <sergebass@gmail.com>
2025-11-14 17:30:42 -0500

    NixOS: gui-lightdm.nix: use relative path addressing for wallpaper image

--- nixos/gui-lightdm.nix
+++ nixos/gui-lightdm.nix
@@ -1,10 +1,6 @@
 # NixOS configuration for lightdm display manager
 
-{ config, pkgs, lib, ... }:
-let
-  wallpaperStore = pkgs.copyPathToStore "${toString ../wallpapers}";
-
-in {
+{ config, pkgs, lib, ... }: {
 
   services = {
     xserver = {
@@ -15,7 +11,7 @@ in {
           enable = true;
 
           # background = "#002020";  # Solid dark teal color as a background
-          background = "${wallpaperStore}/Lake-pebbles.jpg";
+          background = ../wallpapers/Lake-pebbles.jpg;
 
           greeters = {
             gtk = {
