1ed3476f595ba9e4aee319e5d93e62d0eaea5c9f
Sergii Perynskyi <sergebass@gmail.com>
2025-12-08 19:40:49 -0500

    nvim: add nvim-orgmode/orgmode plugin with initial settings

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -246,6 +246,26 @@ return {
     -- (Repo archived) Yet another EditorConfig (http://editorconfig.org) plugin for vim written in vimscript only
     { 'sgur/vim-editorconfig' },
 
+    {
+      'nvim-orgmode/orgmode',
+      event = 'VeryLazy',
+      ft = { 'org' },
+      config = function()
+        -- Setup orgmode
+        require('orgmode').setup({
+          org_agenda_files = '~/orgfiles/**/*',
+          org_default_notes_file = '~/orgfiles/refile.org',
+        })
+
+        -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+        -- add ~org~ to ignore_install
+        -- require('nvim-treesitter.configs').setup({
+        --   ensure_installed = 'all',
+        --   ignore_install = { 'org' },
+        -- })
+      end,
+    },
+
     -- A Vim plugin which shows git diff markers in the sign column and stages/previews/undoes hunks and partial hunks.
     {
         'airblade/vim-gitgutter',
