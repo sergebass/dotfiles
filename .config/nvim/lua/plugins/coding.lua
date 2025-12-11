b1229f1d5fcfac7f749c09a0841d41ee4a4fa5df
Sergii Perynskyi <sergebass@gmail.com>
2025-12-11 00:20:36 -0500

    nvim: dapui: automatically open windows when debugging starts

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -89,6 +89,21 @@ return {
       dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
       config = function()
         require('dapui').setup()
+
+        -- Use nvim-dap events to open and close the windows automatically (:help dap-extensions)
+        local dap, dapui = require("dap"), require("dapui")
+        dap.listeners.before.attach.dapui_config = function()
+          dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+          dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+          dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+          dapui.close()
+        end
       end
     },
 
