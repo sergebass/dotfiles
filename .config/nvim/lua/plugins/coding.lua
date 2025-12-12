1bad196a36480a779aa5515faa5fc3dc0543cb49
Sergii Perynskyi <sergebass@gmail.com>
2025-12-11 20:51:09 -0500

    nvim: move trouble.nvim configuration to coding.lua

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,6 +1,46 @@
 -- Coding-related plugins and configurations for Neovim
 
 return {
+  -- A pretty diagnostics, references, telescope results, quickfix and location list to help you solve all the trouble your code is causing.
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      -- FIXME: revisit keymaps
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Project/Workspace Diagnostics",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics",
+      },
+      -- {
+      --   "<leader>cs",
+      --   "<cmd>Trouble symbols toggle focus=false<cr>",
+      --   desc = "Symbols (Trouble)",
+      -- },
+      -- {
+      --   "<leader>cl",
+      --   "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      --   desc = "LSP Definitions / references / ... (Trouble)",
+      -- },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
+
     -- Quickstart configs for Nvim LSP
     {
       'neovim/nvim-lspconfig',
--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -155,46 +155,6 @@ return {
     end,
   },
 
-  -- A pretty diagnostics, references, telescope results, quickfix and location list to help you solve all the trouble your code is causing.
-  {
-    "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
-    cmd = "Trouble",
-    keys = {
-      -- FIXME: revisit keymaps
-      {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Project/Workspace Diagnostics",
-      },
-      {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics",
-      },
-      -- {
-      --   "<leader>cs",
-      --   "<cmd>Trouble symbols toggle focus=false<cr>",
-      --   desc = "Symbols (Trouble)",
-      -- },
-      -- {
-      --   "<leader>cl",
-      --   "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      --   desc = "LSP Definitions / references / ... (Trouble)",
-      -- },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-      },
-      {
-        "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
-      },
-    },
-  },
-
   -- Fuzzy Finder and more ("Find, Filter, Preview, Pick. All lua, all the time. ")
   {
     'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
