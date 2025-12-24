b0ca1b1424121a4e932eef07bb792e351b79f54b
Sergii Perynskyi <sergebass@gmail.com>
2025-12-24 16:46:51 -0500

    diff-subdirs.sh: generate unique temporary files, for parallelization

--- .local/bin/diff-subdirs.sh
+++ .local/bin/diff-subdirs.sh
@@ -5,12 +5,17 @@
 dir1="$1"
 dir2="$2"
 
-files1="/tmp/a"
-files2="/tmp/b"
+files1=/tmp/$(mktemp diff-subdirs-A-XXXX)
+files2=/tmp/$(mktemp diff-subdirs-B-XXXX)
 
 # Use `tee` to provide visual indication of progress
 time find "$dir1" -type f -printf '%p\n' | sort | xargs -l1 -d '\n' sha256sum | tee "$files1"
 time find "$dir2" -type f -printf '%p\n' | sort | xargs -l1 -d '\n' sha256sum | tee "$files2"
 
+echo "Comparing file hashes: '$files1' vs '$files2'"
+
 # Open neovim in diff mode to see the differences
 nvim -d "$files1" "$files2"
+
+# TODO: Keep temp files on exit (if a specific flag is given)
+rm --verbose "$files1" "$files2"
