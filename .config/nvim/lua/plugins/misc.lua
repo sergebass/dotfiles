0c9209dd519932017c4b081c205bd2347a613338
Sergii Perynskyi <sergebass@gmail.com>
2026-02-02 22:26:41 -0500

    neovim: tabby: do not display windows at the end of tabline

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -42,21 +42,21 @@ return {
                 margin = ' ',
               }
             end),
-            line.spacer(),
-            line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-              return {
-                line.sep('', theme.win, theme.fill),
-                win.is_current() and '' or '',
-                win.buf_name(),
-                line.sep('', theme.win, theme.fill),
-                hl = theme.win,
-                margin = ' ',
-              }
-            end),
-            {
-              line.sep('', theme.tail, theme.fill),
-              { '  ', hl = theme.tail },
-            },
+            -- line.spacer(),
+            -- line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+            --   return {
+            --     line.sep('', theme.win, theme.fill),
+            --     win.is_current() and '' or '',
+            --     win.buf_name(),
+            --     line.sep('', theme.win, theme.fill),
+            --     hl = theme.win,
+            --     margin = ' ',
+            --   }
+            -- end),
+            -- {
+            --   line.sep('', theme.tail, theme.fill),
+            --   { '  ', hl = theme.tail },
+            -- },
             hl = theme.fill,
           }
         end,
