a8b6b39e93330ccc19c27a3d547115425b7144fb
Sergii Perynskyi <sergebass@gmail.com>
2026-02-24 23:49:40 -0500

    gdb: enable Python pretty-printers for standard C++ containers

--- .gdbinit.py
+++ .gdbinit.py
@@ -1,6 +1,7 @@
 # A startup configuration script for gdb in Python
 
 import os
+import sys
 
 print("Loading user configuration (Python)...")
 
@@ -11,3 +12,10 @@ print("Loading user configuration (Python)...")
 #     gdb.execute("set $COLOREDPROMPT = 0")
 # else:
 #     gdb.execute("set $COLOREDPROMPT = 1")
+
+# Enable pretty-printers for standard C++ containers
+print("Registering libstdc++ pretty-printers...")
+sys.path.insert(0, '/usr/share/gcc/python')
+# Note that we need to dynamically load the printers here, we cannot do this at the top level of the script
+from libstdcxx.v6 import register_libstdcxx_printers
+register_libstdcxx_printers(None)
