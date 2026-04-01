444eeb88c7baf1f5bfeb9658110a08dea920bed7
Sergii Perynskyi <sergebass@gmail.com>
2026-03-31 22:22:56 -0400

    fish: use 2 more spaces in fish_title to separate the command from the CWD

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,5 +1,5 @@
 function fish_title
     set -q argv[1]; or set argv fish
     # Example: app --arg1 --arg2  # ~/dotfiles (user@hostname)
-    echo -s $argv "  # " (fish_prompt_pwd_dir_length=0 prompt_pwd) " (" $USER "@"(prompt_hostname) ")"
+    echo -s $argv "    # " (fish_prompt_pwd_dir_length=0 prompt_pwd) " (" $USER "@"(prompt_hostname) ")"
 end
