22d8b28fce4e0a6c4e12286f44dc8ba7abf04a41
Sergii Perynskyi <sergebass@gmail.com>
2026-03-31 22:17:05 -0400

    fish: reset LESS back to `-FR` (use `bat` for proper colors)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,7 +34,8 @@ export PAGER=less
 export MANPAGER=less
 
 # See `man less` for the explanation of the color codes defined with `-D`:
-export LESS="-FR --use-color -Dk+M -Ds+R -Dd+226 -Du+85 -DP229.240*~ -DE200*~ -DS11.18*_ -DN238"
+# export LESS="-FR --use-color -Dk+M -Ds+R -Dd+226 -Du+85 -DP229.240*~ -DE200*~ -DS11.18*_ -DN238"
+export LESS="-FR"
 
 export RIPGREP_CONFIG_PATH=$HOME/.rgrc
 
