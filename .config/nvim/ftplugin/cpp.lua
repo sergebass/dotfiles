1e1a2ad79ff4835e2c48d148b09215339daf115c
Sergii Perynskyi <sergebass@gmail.com>
2026-08-07 18:45:58 -0400

    nvim: cpp and c: use clang-format for both `gq` and `=` formatting

--- .config/nvim/ftplugin/cpp.lua
+++ .config/nvim/ftplugin/cpp.lua
@@ -8,7 +8,12 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.autoindent = true
 
 -- Do our formatting using clang-format and the '=' command
-vim.opt_local.equalprg = "clang-format --assume-filename=" .. vim.fn.expand('%:t')
+vim.opt_local.equalprg = "clang-format --style=file --assume-filename=" .. vim.fn.expand('%:t')
+-- Make `gq` use the equalprg for formatting instead of the LSP formatting function
+vim.opt_local.formatprg = vim.opt_local.equalprg:get()
+-- This variable takes precedence over formatprg, so we need to disable it to use formatprg
+-- (note that we cannot use nil here, since that would make it fall back to the global value, which is set to use LSP formatting)
+vim.opt_local.formatexpr = ''
 
 -- Jump to definition of the symbol under cursor
 -- vim.keymap.set('n', '<CR>', vim.lsp.buf.definition, { buffer = true, desc = 'LSP: Go to definition' })
