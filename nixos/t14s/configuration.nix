64d52344c31862d1442daa7a432958572e438237
Sergii Perynskyi <sergebass@gmail.com>
2026-09-07 19:47:54 -0400

    NixOS (t14s): switch back to LightDM

--- nixos/t14s/configuration.nix
+++ nixos/t14s/configuration.nix
@@ -6,8 +6,8 @@
     ../hardware-common.nix  # Hardware configuration shared across all systems
     ../boot-grub-uefi.nix  # Use GRUB bootloader with UEFI support (more flexible than systemd-boot)
     ../common.nix  # Common configuration shared by all of our NixOS systems
-    # ../gui-lightdm.nix  # LightDM display manager
-    ../gui-sddm.nix  # SDDM display manager (For Plasma/KDE)
+    ../gui-lightdm.nix  # LightDM display manager
+    # ../gui-sddm.nix  # SDDM display manager (For Plasma/KDE)
     ../gui-i3.nix  # i3 X11/GUI environment
     ../gui-icewm.nix  # IceWM X11/GUI environment
     ../gui-plasma.nix  # Plasma X11/GUI environment
