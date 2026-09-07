055a899e293449e8b25c6e238198ccf4106d8fed
Sergii Perynskyi <sergebass@gmail.com>
2026-09-07 18:58:39 -0400

    NixOS (t14s): ssdm: fix build warnings and add uncommented theme settings

--- nixos/gui-sddm.nix
+++ nixos/gui-sddm.nix
@@ -3,18 +3,23 @@
 { config, pkgs, lib, ... }: {
 
   services = {
-    xserver = {
-      displayManager = {
-        startx.enable = lib.mkForce false;  # Force-disable startx when using a display manager
+    xserver.displayManager.startx.enable = lib.mkForce false;  # Force-disable startx when using a display manager
+    displayManager = {
+      sddm = {
+        enable = true;
 
-        sddm = {
-          enable = true;
-
-          wayland = {
-            enable = true;  # Experimental Wayland support
-          };
+        wayland = {
+          enable = true;  # Experimental Wayland support
         };
+
+        # theme = "${pkgs.sddm-chili-theme}/share/sddm/themes/chili";
       };
     };
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      # sddm-chili-theme
+    ];
   };
 }
